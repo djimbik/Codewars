d51fb07b8f8f1c42430d5e98464f84a8a145a208
djimbik <djimbik93@gmail.com>
2023-01-05 13:25:02 +0300

    new task min, max (spread)

--- [codewars] 8,7,6 kyu - arrays.js
+++ [codewars] 8,7,6 kyu - arrays.js
@@ -7,3 +7,23 @@ function gooseFilter (birds) {
     // return an array containing all of the strings in the input array except those that match strings in geese
   };
   gooseFilter(["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"])
+
+//new 
+//You solution should return an array [smallest, minimumAbsent, largest]
+// The minimumAbsent is the smallest number between the largest and the smallest number that is not in the array.
+
+function minMinMax(array) {
+    const largest = Math.max(...array);
+    const smallest = Math.min(...array);
+     
+    let minimumAbsent;
+    
+     for (let i = smallest; i < largest; i++) {
+       if (!array.includes(i)) {
+        minimumAbsent = i;
+        return [smallest, minimumAbsent, largest]
+       }
+     };
+}
+
+console.log(minMinMax([-23, -22, 24]));
