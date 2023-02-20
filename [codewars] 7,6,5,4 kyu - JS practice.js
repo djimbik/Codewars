2282fcbe99fedb0cd9a450e00b5d2596e6ed1321
djimbik <djimbik93@gmail.com>
2023-02-20 07:27:26 +0300

    1 task 7 kyu

--- [codewars] 7,6,5,4 kyu - JS practice.js
+++ [codewars] 7,6,5,4 kyu - JS practice.js
@@ -954,3 +954,33 @@ class Quark{
     [this.color, other.color] = [other.color, this.color]
   }
 }
+
+//48 task
+//[7 kyu]
+// Building blocks
+// https://www.codewars.com/kata/55b75fcf67e558d3750000a3
+
+class Block{
+
+  constructor(data){
+    this.width = data[0];
+    this.length = data[1];
+    this.height = data[2];
+  }
+  
+  getWidth() {
+    return this.width
+  }
+  getLength() {
+    return this.length
+  }
+  getHeight() {
+    return this.height
+  }
+  getVolume() {
+    return this.width * this.length * this.height
+  }
+  getSurfaceArea() {
+    return 2 * (this.width * this.length + this.width * this.height + this.length * this.height)
+  }
+}
